d5badb0efd835c142f91133b864c2a1d1cacd2aa
se7ensecondz <se7ensecondz@gmail.com>
2024-06-24 00:47:03 -0700

    update

--- src/backend/prometheus_metrics/__init__.py
+++ src/backend/prometheus_metrics/__init__.py
@@ -1,4 +1,8 @@
+from pathlib import Path
 from prometheus_client import make_asgi_app, CollectorRegistry, multiprocess
+
+import os
+os.environ['PROMETHEUS_MULTIPROC_DIR'] = str(Path(__file__).parent / 'metrics')
 
 
 # Using multiprocess collector for registry
