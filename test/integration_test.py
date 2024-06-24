4ddca4a994a8b4f0da1c5aa38d8e0672a7fd0298
se7ensecondz <se7ensecondz@gmail.com>
2024-06-24 01:11:36 -0700

    update

--- test/integration_test.py
+++ test/integration_test.py
@@ -16,7 +16,7 @@ def test_integration_test():
     url = "https://api.themoviedb.org/3/genre/movie/list?language=en"
     headers = {
         "accept": "application/json",
-        "Authorization": "Bearer " + os.getenv('BEARER_TOKEN')
+        "Authorization": "Bearer " + os.environ['BEARER_TOKEN']
     }
     response = requests.get(url, headers=headers)
     genres = response.json().get('genres')
